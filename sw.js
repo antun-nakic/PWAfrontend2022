84deaac6dd3da241e250830e53728c3dd1ed524f
Antun Nakić <antun.nakic@digital-abundance.hr>
2022-07-06 21:19:52 +0200

    gotovo valjda

--- sw.js
+++ sw.js
@@ -1,12 +1,39 @@
 const cacheName = "nasPrviPWA-v1";
 
+const OFFLINE_URL = "/WeatherApp_API/index.html";
+
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install");
   e.waitUntil(
     (async () => {
       const cache = await caches.open(cacheName);
       console.log("[Service Worker] Caching all: app shell and content");
-      await cache.addAll([{ a: 1 }, { b: 2 }]);
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+    })()
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      try {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) {
+          return r;
+        }
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      } catch (error) {
+        console.log("Fetch failed; returning offline page instead.", error);
+
+        const cache = await caches.open(cacheName);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
     })()
   );
 });
